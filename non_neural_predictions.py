f2f581406a35dc86b1e7f1313b6ec1a4bc9da956
DivyanshuSaxena <dssaxena2011@gmail.com>
2020-04-29 01:56:00 +0530

    Added basic code for getting district data and running episodic model over it

--- non_neural_predictions.py
+++ non_neural_predictions.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
+# In[81]:
 
 
 # Imports
 import numpy as np
 import pandas as pd
+import importlib as imp
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
+from sklearn.metrics import accuracy_score
 
 
-# In[16]:
+# In[46]:
+
+
+# User module imports
+from utils import district_daily_data as dd
+dd = imp.reload(dd)
+
+
+# In[52]:
 
 
 # Flags
-linear_reg = True
+linear_reg = False
 sv_reg = True
+episodes = True
 
 
-# In[6]:
+# In[41]:
 
 
 # Directory variables
 data_dir = 'data/'
 
 
-# In[8]:
+# In[32]:
 
 
 # Read state data
@@ -38,7 +49,19 @@ arr_state = np.cumsum(arr_state, axis=0) # now cumulative cases till day 52
 np.savetxt(data_dir + 'state-date-total-data-cumulative.csv', arr_state.astype(int), fmt='%i', delimiter=",")
 
 
-# In[17]:
+# In[55]:
+
+
+# Read district data
+districts = dd.get_all_districts()
+dist_series = []  # [(start_date, series), (start_date, series), ...]
+# Note: start_date might itself be a feature
+for d in districts:
+    d_start_date = dd.get_infection_start(d)
+    dist_series.append((d_start_date, dd.get_district_time_series(d, d_start_date)))
+
+
+# In[39]:
 
 
 if linear_reg:
@@ -61,7 +84,7 @@ if linear_reg:
 # In[43]:
 
 
-if sv_reg:
+if sv_reg and not dist_reg:
     # Get x and y plots - SVRegression
     X = np.arange(1,53)
     X = np.reshape(X, (52,1))
@@ -76,6 +99,40 @@ if sv_reg:
         plt.scatter(X, y, color='black')
         plt.plot(x_test,y_test)
         plt.show()
+
+
+# In[87]:
+
+
+if dist_reg:
+    # Construct train and test data and fit Support Vector Regression
+    x = []
+    y = []
+    episode_length = 7
+    count = 0
+    for tup in dist_series:
+        series = tup[1]
+        num_episodes = len(series) - episode_length + 1
+        if num_episodes < 2: continue
+        for _in in range(num_episodes-1):
+            x.append(series[_in:_in+episode_length])
+            y.append(series[_in+episode_length])
+    x = np.array(x)
+    y = np.array(y)
+
+    train_length = int(0.8*len(x))
+    x_train = x[:train_length]
+    y_train = y[:train_length]
+    x_test = x[train_length:]
+    y_true = y[train_length:]
+    clf = SVR(C=1.0, gamma='auto')
+    clf.fit(x_train, y_train)
+    
+    y_test = clf.predict(x_test)
+    X = np.arange(len(y_test))
+    X = np.reshape(X, (len(y_test), 1))
+    plt.plot(X, y_true, color='black')
+    plt.plot(X, y_test, color='red')
 
 
 # In[ ]:
--- utils/district_daily_data.py
+++ utils/district_daily_data.py
@@ -1,9 +1,11 @@
 """Expose the API for the District Daily Data"""
 # Reads the data from the district json file and returns values
+import os
 import json
 from datetime import datetime as dt
 
-data_dir = '../data/'
+script_dir = os.path.dirname(os.path.abspath(__file__))
+district_data_dir = script_dir + '/../data/'
 date_format = '%d/%m/%Y'
 
 
@@ -17,6 +19,23 @@ def convert_to_daily(data_list):
     data_list[-_in] = data_list[-_in] - data_list[-_in - 1]
 
 
+def get_infection_start(district):
+  """Get the date of infection start in the district
+
+  Arguments:
+      district {String} -- Name of the district
+
+  Returns:
+      String -- Start date of infection
+  """
+  with open(district_data_dir + 'district-data.json') as f:
+    district_dict = json.load(f)
+
+  for date, data in district_dict.items():
+    if district in data:
+      return date
+
+
 def get_district_data(district):
   """Returns the raw data of the given district in a list
 
@@ -26,7 +45,7 @@ def get_district_data(district):
   Returns:
       List -- [(date, count), (date, count), ..., (date, count)]
   """
-  with open(data_dir + 'district-data.json') as f:
+  with open(district_data_dir + 'district-data.json') as f:
     district_dict = json.load(f)
 
   district_data = []
@@ -41,8 +60,8 @@ def get_district_data(district):
 
 def get_district_time_series(district,
                              start_date,
-                             num_days=-1,
-                             cumulative=False):
+                             cumulative=False,
+                             num_days=-1):
   """Return the time series data of the given district
 
   Arguments:
@@ -50,8 +69,8 @@ def get_district_time_series(district,
       start_date {String} -- Start Date in format DD/MM/YYYY
 
   Keyword Arguments:
-      num_days {Integer} -- Number of days (default: {-1, to get all days data})
       cumulative {bool} -- Whether cumulative data is needed (default: {False})
+      num_days {Integer} -- Number of days (default: {-1, to get all days data})
 
   Returns:
       List -- List of requisite numbers. Length = num_days
@@ -59,23 +78,29 @@ def get_district_time_series(district,
   district_data = get_district_data(district)
 
   ret_list = []
-  prev_date = start_date
+  prev_date = dt.strptime(start_date, date_format)
   for obj in district_data:
+    curr_date = dt.strptime(obj[0], date_format)
     # Condition to check if ret_list has been initiated.
     # Only then check if some days got missing in between.
     if ret_list:
       # Calculate number of days between prev_date and curr date
-      days = (dt.strptime(obj[0], date_format) -
-              dt.strptime(prev_date, date_format)).days
+      days = (curr_date - prev_date).days
       if days > 1:
         # Append the last number again for those many days
         for _in in range(days - 1):
           ret_list.append(ret_list[-1])
 
-    if obj[0] == start_date or ret_list:
+    # Add data for the range given
+    if curr_date >= dt.strptime(start_date, date_format):
+      # Case: When the given start date is before the start of infections
+      if not ret_list:
+        initial_days = (curr_date - dt.strptime(start_date, date_format)).days
+        for _in in range(initial_days):
+          ret_list.append(0)
       ret_list.append(obj[1])
 
-    prev_date = obj[0]
+    prev_date = curr_date
     if len(ret_list) == num_days:
       break
 
@@ -84,13 +109,13 @@ def get_district_time_series(district,
   return ret_list
 
 
-def get_district_list():
+def get_all_districts():
   """Get the list of all the districts in the district-data api
 
   Returns:
       Set -- Set of all districts
   """
-  with open(data_dir + 'district-data.json') as f:
+  with open(district_data_dir + 'district-data.json') as f:
     district_dict = json.load(f)
   districts = set([])
 
@@ -107,7 +132,7 @@ def get_district_list():
 
 
 if __name__ == '__main__':
-  d = get_district_time_series('Delhi', '04/03/2020')
+  d = get_district_time_series('Alappuzha', '04/03/2020', True)
   print(d)
-  s = get_district_list()
+  s = get_all_districts()
   print(len(s))
